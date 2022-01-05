b4b76cb1e1f38672843883570fca6e5b4528e65a
Klaudia <klaudia.diana.paluch@gmail.com>
2022-01-05 11:43:17 +0100

    init

---
+++ main.js
@@ -0,0 +1,104 @@
+const colorArray = [
+  "#FF6633",
+  "#FFB399",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+  "#3366E6",
+  "#999966",
+  "#99FF99",
+  "#B34D4D",
+  "#80B300",
+  "#809900",
+  "#E6B3B3",
+  "#6680B3",
+  "#66991A",
+  "#FF99E6",
+  "#CCFF1A",
+  "#FF1A66",
+  "#E6331A",
+  "#33FFCC",
+  "#66994D",
+  "#B366CC",
+  "#4D8000",
+  "#B33300",
+  "#CC80CC",
+  "#66664D",
+  "#991AFF",
+  "#E666FF",
+  "#4DB3FF",
+  "#1AB399",
+  "#E666B3",
+  "#33991A",
+  "#CC9999",
+  "#B3B31A",
+  "#00E680",
+  "#4D8066",
+  "#809980",
+  "#E6FF80",
+  "#1AFF33",
+  "#999933",
+  "#FF3380",
+  "#CCCC00",
+  "#66E64D",
+  "#4D80CC",
+  "#9900B3",
+  "#E64D66",
+  "#4DB380",
+  "#FF4D4D",
+  "#99E6E6",
+  "#6666FF",
+];
+const sounds = [
+  { sound: "assets/sounds/bell.wav", icon: "assets/images/bell.png" },
+  { sound: "assets/sounds/bird.wav", icon: "assets/images/bird.png" },
+  { sound: "assets/sounds/dog.wav", icon: "assets/images/dog.png" },
+  { sound: "assets/sounds/kiss.wav", icon: "assets/images/kiss.png" },
+  { sound: "assets/sounds/laugh.wav", icon: "assets/images/laugh.png" },
+  { sound: "assets/sounds/lion.wav", icon: "assets/images/lion.png" },
+  { sound: "assets/sounds/guitar.wav", icon: "assets/images/guitar.png" },
+  { sound: "assets/sounds/cat.wav", icon: "assets/images/cat.png" },
+  { sound: "assets/sounds/horse.wav", icon: "assets/images/horse.png" },
+];
+const container = document.querySelector(".container");
+
+function getRandomColor(array) {
+  let min = Math.ceil(0);
+  let max = Math.floor(array.length);
+  let random = Math.floor(Math.random() * (max - min)) + min;
+  return array[random];
+}
+function applyRandomColor(element) {
+  element.style.backgroundColor = getRandomColor(colorArray);
+  return element;
+}
+function addColorEffects(element) {
+  element.addEventListener("mouseenter", (e) => {
+    applyRandomColor(element);
+  });
+  element.addEventListener("mousedown", (e) => {
+    applyRandomColor(element);
+  });
+}
+function createButton(icon, sound) {
+  let div = document.createElement("div");
+  div.className = "soundBtn";
+  let image = document.createElement("img");
+  image.src = icon;
+  div.appendChild(image);
+  applyRandomColor(div);
+  addColorEffects(div);
+  addSoundEffect(div, sound);
+  container.appendChild(div);
+  return div;
+}
+function addSoundEffect(element, track) {
+  var audio = new Audio(track);
+  element.addEventListener("click", (e) => {
+    audio.play();
+  });
+}
+sounds.forEach((item) => {
+  createButton(item.icon, item.sound);
+});
